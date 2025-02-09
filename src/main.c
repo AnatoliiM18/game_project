649e6f361d84f0e10fb0f87541c8daad08b48283
AnatoliiM18 <mamaev-av18@ya.ru>
2025-02-09 21:32:56 +0400

    Add extended SDL_Log macro

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+/* Extended SDL_Log() macro (includes file name and line number) */
+#define LOG_ERROR(func_name)    do {                                    \
+                                    SDL_Log("[%s:%d] %s error: %s",     \
+                                            __FILE__, __LINE__,         \
+                                            #func_name, SDL_GetError());\
+                                } while (0)
+
 /* Global defines */
 #define WND_TITLE   "Window title"
 #define WND_WIDHT   640
@@ -10,16 +17,17 @@
 /* Globals */
 static SDL_Window* g_window;
 
-
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
+        LOG_ERROR(SDL_Init);
         return SDL_APP_FAILURE;
     }
 
     g_window = SDL_CreateWindow(WND_TITLE, WND_WIDHT, WND_HEIGHT, SDL_WINDOW_RESIZABLE);
 
     if (!g_window) {
+        LOG_ERROR(SDL_CreateWindow);
         return SDL_APP_FAILURE;
     }
 
