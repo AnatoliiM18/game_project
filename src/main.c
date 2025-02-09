fdac62faa95689652b5c18a38ddc9ae3916dd957
AnatoliiM18 <mamaev-av18@ya.ru>
2025-02-09 21:13:09 +0400

    Minimal SDL program

--- src/main.c
+++ src/main.c
@@ -1,4 +1,45 @@
-int main(int argc, char* argv[])
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+/* Global defines */
+#define WND_TITLE   "Window title"
+#define WND_WIDHT   640
+#define WND_HEIGHT  480
+
+/* Globals */
+static SDL_Window* g_window;
+
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    return 0;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return SDL_APP_FAILURE;
+    }
+
+    g_window = SDL_CreateWindow(WND_TITLE, WND_WIDHT, WND_HEIGHT, SDL_WINDOW_RESIZABLE);
+
+    if (!g_window) {
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
 }
