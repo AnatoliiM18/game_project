91a84b0b117e16e7c9eb993f0aedcf7df31b990f
AnatoliiM18 <mamaev-av18@ya.ru>
2025-02-13 00:58:10 +0400

    Draw text (SDL3_ttf)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_ttf\SDL_ttf.h>
 
 /* Extended SDL_Log() macro (includes file name and line number) */
 #define LOG_ERROR(func_name)    do {                                    \
@@ -17,6 +18,7 @@ static SDL_Window* g_window;
 static SDL_Renderer* g_renderer;
 static int g_wnd_width;
 static int g_wnd_height;
+SDL_Texture* g_text_texture;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
@@ -29,12 +31,32 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         return SDL_APP_FAILURE;
     }
 
+    if (!TTF_Init()) {
+        LOG_ERROR(TTF_Init);
+        return SDL_APP_FAILURE;
+    }
+
     if (!SDL_CreateWindowAndRenderer(WND_TITLE, g_wnd_width, g_wnd_height,
         SDL_WINDOW_RESIZABLE, &g_window, &g_renderer)) {
 
         LOG_ERROR(SDL_CreateWindowAndRenderer);
         return SDL_APP_FAILURE;
     }
+
+    /* Work with text */
+    TTF_Font* font = TTF_OpenFont("../assets/Xolonium-Bold.ttf", 36.0f);
+    if (!font) {
+        LOG_ERROR(TTF_OpenFont);
+        return SDL_APP_FAILURE;
+    }
+
+    const char* text = "Test string.*+- 1 2 ! 5 90 O % / \\ 0 6 j i";
+    SDL_Color text_color = { 0xFF, 0x80, 0x80, SDL_ALPHA_OPAQUE };
+    SDL_Surface* text_surf = TTF_RenderText_Solid(font, text, 0, text_color);
+    g_text_texture = SDL_CreateTextureFromSurface(g_renderer, text_surf);
+
+    TTF_CloseFont(font);
+    SDL_DestroySurface(text_surf);
 
     return SDL_APP_CONTINUE;
 }
@@ -62,7 +84,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
     SDL_FRect rect;
-
+    
     /* Clear the current rendering target at the start of every frame */
     SDL_SetRenderDrawColor(g_renderer, 0x00, 0x80, 0x80, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(g_renderer);
@@ -79,14 +101,22 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     SDL_RenderFillRect(g_renderer, &rect);
 
     /* Draw grid */
-    SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
-    float grid_step = 20.0f;
+    SDL_SetRenderDrawColor(g_renderer, 0x80, 0x80, 0x80, SDL_ALPHA_OPAQUE);
+    float grid_step = 10.0f;
     for (float y_pos = 0.0f; y_pos < g_wnd_height; y_pos += grid_step) {
         SDL_RenderLine(g_renderer, 0.0f, y_pos, g_wnd_width, y_pos);
     }
     for (float x_pos = 0.0f; x_pos < g_wnd_width; x_pos += grid_step) {
         SDL_RenderLine(g_renderer, x_pos, 0.0f, x_pos, g_wnd_height);
     }
+
+    /* Draw text */
+    SDL_FRect dst_text_rect;
+    dst_text_rect.x = rect.x;
+    dst_text_rect.y = rect.y;
+    dst_text_rect.w = g_text_texture->w;
+    dst_text_rect.h = g_text_texture->h;
+    SDL_RenderTexture(g_renderer, g_text_texture, NULL, &dst_text_rect);
 
     SDL_RenderPresent(g_renderer);
 
@@ -95,4 +125,5 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
+    TTF_Quit();
 }
